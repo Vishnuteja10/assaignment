cfc80a5111f8922b41923418988c9d2b21f8a641
Vishnuteja <vishnuteja476@gamil.com>
2022-12-13 15:16:11 +0530

    b1

---
+++ Memory-Assistant-master/app/src/main/java/com/memory_athlete/memoryassistant/NotificationReceiver.java
@@ -0,0 +1,12 @@
+package com.memory_athlete.memoryassistant;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
